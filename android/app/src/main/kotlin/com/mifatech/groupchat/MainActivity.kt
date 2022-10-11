10a99efa28b82874ba047a17a53430592b56c856
Mintesnot Fikir <mints4god@gmail.com>
2022-10-11 10:53:41 +0300

    RE

---
+++ android/app/src/main/kotlin/com/mifatech/groupchat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mifatech.groupchat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/shared/constants.dart
@@ -0,0 +1,7 @@
+class Constants {
+  static String appId = '1:643682376578:web:d948d7d90d18f9b3348d9b';
+  static String apiKey = 'AIzaSyCZPbw-m0ecrI6hGGirz_lGa0d_vb4SvBc';
+  static String messagingSenderId = '643682376578';
+  static String projectId = 'groupchatflutter-af3e3';
+}
+ 
